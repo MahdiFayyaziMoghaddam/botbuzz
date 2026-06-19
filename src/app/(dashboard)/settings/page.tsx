806e6f33ae5c679a30d0bfc4e1da4d6e1f8577ef
MahdiFayyaziMoghaddam <mfm.8768h@gmail.com>
2026-06-19 16:42:54 +0330

    Complete settings section

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,3 +1,12 @@
+import Switch from "@/components/input/Switch";
+
 export default function Settings() {
-	return <></>;
+	return (
+		<div className="p-24 max-xl:p-20 max-lg:p-16 max-md:p-12 max-sm:p-10 max-xs:p-8">
+			<div className="flex justify-between items-center">
+				<h5>Notifications</h5>
+				<Switch />
+			</div>
+		</div>
+	);
 }
