8fcaf00e77531c26fa08e1deb592977a6cf8eed2
MahdiFayyaziMoghaddam <mfm.8768h@gmail.com>
2026-07-30 12:38:41 +0330

    Remove supabase client from proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,32 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import { createServerClient } from "@supabase/ssr";
-import { env } from "./utils/env";
 
 export async function proxy(request: NextRequest) {
-	const { SUPABASE_URL, SUPABASE_KEY } = env();
-
 	const headers = new Headers(request.headers);
 
 	headers.append("x-pathname", request.nextUrl.pathname);
 
-	let response = NextResponse.next({
+	const response = NextResponse.next({
 		request: {
 			headers
-		}
-	});
-
-	createServerClient(SUPABASE_URL, SUPABASE_KEY, {
-		cookies: {
-			getAll() {
-				return request.cookies.getAll();
-			},
-			setAll(cookiesToSet) {
-				cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
-				response = NextResponse.next({
-					request
-				});
-				cookiesToSet.forEach(({ name, value, options }) => response.cookies.set(name, value, options));
-			}
 		}
 	});
 
