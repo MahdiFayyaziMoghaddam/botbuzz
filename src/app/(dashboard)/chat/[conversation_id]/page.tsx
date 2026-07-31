dd290a0fe1b3b58c3c819af6f33d85a1eb182d4a
MahdiFayyaziMoghaddam <mfm.8768h@gmail.com>
2026-07-31 19:06:30 +0330

    Fix title of dynamic chat page

--- src/app/(dashboard)/chat/[conversation_id]/page.tsx
+++ src/app/(dashboard)/chat/[conversation_id]/page.tsx
@@ -1,5 +1,9 @@
 import Client from "./_client";
 
+export const generateMetadata = () => {
+	return { title: "Chat" };
+};
+
 export default async function ChatSession({ params }: { params: Promise<{ conversation_id: string }> }) {
 	const { conversation_id } = await params;
 	return <Client conversation_id={conversation_id} />;
