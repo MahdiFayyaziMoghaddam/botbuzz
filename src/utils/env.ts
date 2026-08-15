aec0783f5e69a37786e54a11e348721f45452bc2
MahdiFayyaziMoghaddam <mfm.8768h@gmail.com>
2026-08-16 02:55:57 +0330

    Update env util

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,11 +1,15 @@
 export function env() {
 	const SUPABASE_URL = process.env.SUPABASE_URL;
 	const SUPABASE_KEY = process.env.SUPABASE_KEY;
+	const GOOGLE_GENERATIVE_AI_API_KEY = process.env.GOOGLE_GENERATIVE_AI_API_KEY;
 	if (!SUPABASE_URL) {
 		throw new Error("Missing SUPABASE_URL environment variable.");
 	}
 	if (!SUPABASE_KEY) {
 		throw new Error("Missing SUPABASE_KEY environment variable.");
 	}
-	return { SUPABASE_URL, SUPABASE_KEY };
+	if (!GOOGLE_GENERATIVE_AI_API_KEY) {
+		throw new Error("Missing GOOGLE_GENERATIVE_AI_API_KEY environment variable.");
+	}
+	return { SUPABASE_URL, SUPABASE_KEY, GOOGLE_GENERATIVE_AI_API_KEY };
 }
